c10f5c8520c4ad85c7ba16cca89edebedc4eb535
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-29 21:20:05 -0600

    trying to make a cool thing

--- src/components/RestProfile.vue
+++ src/components/RestProfile.vue
@@ -47,24 +47,46 @@
                    
                         <input placeholder="upload your image here" type="text">
                    
+                        
                    
                     </span>
+
+                    <article>
+
 
                     <span>
 
                         <h1>Add, modify, or remove your menu items:</h1>
 
-                        <p>Name</p>
+
+                        <div>
+                            <img :src="menu_image[this.index]">
+
+                            <button @click="Next">Next</button>
+                         
+                            
+
+                           
+
+
+
+                        
+                        </div>
+
+
+                        <p></p>
                         <input type="text" ref="Name_Box"> 
-                        <p>Description</p>
+                        <p></p>
                         <input type="text" ref="Desc_Box"> 
-                        <p>Image</p>
+                        <p></p>
                         <input type="text" ref="Image_Box"> 
-                        <p>Price</p>
+                        <p></p>
                         <input type="text" ref="Price_Box"> 
   
                         <button @click="add_product">Add</button><button @click="edit_product">Edit</button><button @click="delete_product">Delete</button>
                     </span>
+
+                    </article>
 
                     <span>
                         
@@ -72,6 +94,7 @@
                     
                         <button>Delete Account</button>
                     
+                       
                     </span>
                 </article>
             </section>
@@ -89,10 +112,16 @@ import Cookies from 'vue-cookies';
         data() {
             return {
                 
+                index: 0,
+
 
             rest_data_holder: {},
 
-            menu_get_holder: []
+            menu_get_holder: [],
+
+            menu_image: []
+
+
            
         }
         },
@@ -159,51 +188,6 @@ import Cookies from 'vue-cookies';
                 let restaurant_token = Cookies.get(`rest_login_token`);
 
 
-                let rest_id_value = Cookies.get(`restaurant_id`);
-
-                if(rest_id_value !== undefined){
-
-
-            
-                    axios({
-
-                        method: `GET`,
-
-                        url: `https://foodie.bymoen.codes/api/menu`,
-
-                        headers:{
-
-                        'x-api-key': `qK2iR1gTkkAjPH0kfGDY`,
-                    
-                        },
-                        
-                        params:{
-
-                        restaurant_id: rest_id_value,
-
-                        },
-                     }).then((response)=>{
-
-                        response;
-
-                        for(let i = 0; i < response[`data`].length; i = i +1){
-
-                            this.menu_get_holder.push(response[`data`][i]);
-
-                           
-                        }
-
-                    }).catch((error)=>{
-
-                        error;
-
-                    });
-
-                }
-
-                if(this.menu_get_holder[`id`] !== undefined){
-
-
                     axios({
 
                         method: `PATCH`,
@@ -221,6 +205,7 @@ import Cookies from 'vue-cookies';
                     
                     data:{
 
+               
 
                         name: this.$refs[`Name_Box`][`value`],
                         description: this.$refs[`Desc_Box`][`value`],
@@ -248,11 +233,25 @@ import Cookies from 'vue-cookies';
                     
 
 
-                }
+                
            
                 
 
             },
+
+
+            Next(){
+
+                this.index++;
+                
+                if(this.index > this.menu_image.length -1){
+
+                    this.index = 0;
+                }
+
+    
+            },
+
 
             delete_product(){
 
@@ -316,6 +315,55 @@ import Cookies from 'vue-cookies';
             });
 
         }
+
+                if(rest_id_value !== undefined){
+
+
+            
+                    axios({
+
+                        method: `GET`,
+
+                        url: `https://foodie.bymoen.codes/api/menu`,
+
+                        headers:{
+
+                        'x-api-key': `qK2iR1gTkkAjPH0kfGDY`,
+                    
+                        },
+                        
+                        params:{
+
+                        restaurant_id: rest_id_value,
+
+                        },
+                     }).then((response)=>{
+
+                        response;
+
+                        for(let i = 0; i < response[`data`].length; i = i +1){
+
+                            this.menu_get_holder.push(response[`data`][i]);
+
+                        
+                            this.menu_image.push(response[`data`][i][`image_url`]);
+
+
+                            
+                           
+                        }
+
+                    }).catch((error)=>{
+
+                        error;
+
+                    });
+                    
+
+                }
+         
+                console.log(this.menu_image);
+
         }
     }
 </script>

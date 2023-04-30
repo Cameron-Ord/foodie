e8c77a96700a3c7727898d79a159da911ee11dca
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-30 00:24:22 -0600

    working on public page

--- src/components/PublicPageComp.vue
+++ src/components/PublicPageComp.vue
@@ -1,4 +1,6 @@
 <template>
+    <div>
+    <article>
     <span>
 
         <h1>{{ restaurant_object[`name`] }}</h1>
@@ -14,10 +16,22 @@
       <p>{{ restaurant_object[`bio`] }}</p>
 
 
+    </span>
+</article>
+<article>
 
+    <span v-for="(menu_item, i) in rest_menu" :key="i">
 
+        <h1>{{ menu_item[`name`] }}</h1>
+
+        <img :src="menu_item[`image_url`]">
+
+        <p>{{ menu_item[`price`] }}</p>
 
     </span>
+
+</article>
+</div>
 </template>
 
 <script>
@@ -28,7 +42,9 @@ import Cookies from 'vue-cookies';
 
         data() {
             return {
-               restaurant_object: {}
+               restaurant_object: {},
+
+               rest_menu: []
             }
         },
 
@@ -75,8 +91,44 @@ import Cookies from 'vue-cookies';
 
             }
 
+            if(chosen_restaurant_id !== undefined){
 
-          
+            axios({
+
+                method: `GET`,
+
+                url: `https://foodie.bymoen.codes/api/menu`,
+
+                
+                    headers:{
+
+                        'x-api-key': `qK2iR1gTkkAjPH0kfGDY`,
+                    },
+
+                    params:{
+
+                        restaurant_id: chosen_restaurant_id
+
+                    }
+
+
+            }).then((response)=>{
+
+                console.log(response)
+
+                for (let i = 0; i < response[`data`].length; i++){
+
+                    this.rest_menu.push(response[`data`][i]);
+                }
+
+                console.log(this.rest_menu);
+
+            }).catch((error)=>{
+
+                error
+
+            })
+        }
 
 
         }
